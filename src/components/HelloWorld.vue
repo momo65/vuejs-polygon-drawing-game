b664f1c3c0093ea8b5331a283923d5a87008a2e4
momo65 <mohamed.zerzeri2@gmail.com>
2018-11-08 17:55:10 +0100

    just finished the polygon drawing app, still not perfect, the lines can intersect with each others

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,25 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <md-card>
+      <div class="row">
+        <div class="drawingBoard col-sm-8 col-md-8 col-xs-12">
+          <svg id="svg" style="width:100%;height:100%;" v-on:click="drawLine"></svg>
+        </div>
+        <div class="md-layout md-gutter listDiv col-sm-2 col-md-2 col-xs-10">
+          <div class="md-layout-item">
+            <md-field>
+              <label for="stroke">Color</label>
+              <md-select v-model="stroke" name="strokeColor" id="stroke" md-dense>
+                <md-option v-for="color in colors" :value="color">{{color}}</md-option>
+              </md-select>
+            </md-field>
+          </div>
+        </div>
+      </div>
+      <br><br>
+      <md-button class="md-dense md-raised md-primary d-block" v-bind:disabled="!gameStarted"
+        v-on:click="restartGame">Restart game</md-button>
+    </md-card>
   </div>
 </template>
 
@@ -35,24 +28,120 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },methods:{
+    restartGame:function(){//it clears the drawing board & restarts the game
+      this.$data.points=[]
+      this.$data.gameOver=false
+      this.$data.gameStarted=false
+      this.$data.index=0;
+      this.$data.strokeW="1"
+      this.$data.s.clear()
+    },
+    drawLine:function(event){//it calculates everything to draw the next line & make the necessary verifications
+      if(this.$data.index==0){
+        this.$data.gameStarted=true
+      }
+      if(!this.$data.gameOver){
+        let point={x:event.offsetX,y:event.offsetY}
+        this.$data.points.push(point)
+        if(this.$data.index>0){
+          this.$data.s.line(this.$data.points[this.$data.index-1].x,this.$data.points[this.$data.index-1].y,
+            this.$data.points[this.$data.index].x,this.$data.points[this.$data.index].y).attr({stroke:this.$data.stroke,
+            strokeWidth:"1"})
+        }
+        this.$data.index++;
+        if(this.$data.index>=3){ //ending of the game possible from index>=3
+          if(this.checkRadius(this.$data.points[0],this.$data.points[this.$data.points.length-1])){
+            this.$data.gameOver=true
+          }
+        }
+      }
+    },
+    checkRadius:function(firstP,lastP){
+      //it checks if the last point is near enough to the first point to decide if we close the pentagon or not
+
+      if(Math.abs(firstP.x-lastP.x)<=9 && Math.abs(firstP.y-lastP.y)<=9){
+
+        //9 is the max radius of the circle that might contain the closing point
+
+        //the possible equations that i thought about that might calculate the fitting strokeWidth.
+
+        this.$data.strokeW=""+(2*(Math.abs(firstP.x-lastP.x)+Math.abs(firstP.y-lastP.y)))
+
+        /*if(Math.abs(firstP.x-lastP.x)>= Math.abs(firstP.y-lastP.y)){
+          this.$data.strokeW=""+(2*Math.abs(firstP.x-lastP.x))
+        }else{
+          this.$data.strokeW=""+(2*Math.abs(firstP.y-lastP.y))
+        }*/
+
+        this.adjustPentagon()
+        return true
+      }else{
+        return false
+      }
+    },
+    adjustPentagon:function(){ //it redraws the pentagon another time with the calculated strokeWidth
+      for(let k=1;k<this.$data.points.length;k++){
+      this.$data.s.line(this.$data.points[k-1].x,this.$data.points[k-1].y,
+        this.$data.points[k].x,this.$data.points[k].y).attr({stroke:this.$data.stroke,
+        strokeWidth:this.$data.strokeW})
+      }
+    }
+  },
+  data(){
+    return{
+      s:null,  //the snap variable
+      points:[],
+      index:0,//index pointing on the last point.
+      gameOver:false,
+      strokeW:"1",
+      stroke:"black",
+      gameStarted:false,
+      colors:['Black','Green','Red','Blue','Brown','Violet','Orange','Yellow',  //possible colors to draw with
+        'Aqua','Aquamarine','Azure','Crimson','Cyan','DarkBlue','DarkCyan','DarkGoldenRod',
+        'FireBrick','FloralWhite','ForestGreen','Fuchsia','Gainsboro','Khaki','Lavender','LavenderBlush','LawnGreen','LemonChi',
+        'MediumSeaGreen','MediumSlateBlue','MediumSpringGreen','MediumTurquoise','MediumVioletRed','PaleGreen','PaleTurquoise',
+        'PaleVioletRed','PapayaWhip','PeachPu','Peru']
+    }
+  },
+  mounted(){  //require Snap & create the snap variable after the component mounted
+    const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`)
+    this.$data.s=Snap("#svg")
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.md-card{
+  width:860px;
+  height:550px;
+  margin-left:60px;
+  margin-top:8px;
+  bottom:20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+svg{
+  background-color: #F9F9F9;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.drawingBoard{
+  position:relative;
+  width:570px;
+  height:386px;
+  margin-left:16px;
+  margin-top:8px;
+  top:24px;
 }
-a {
-  color: #42b983;
+.listDiv{
+  width:66px !important;
+  margin-left:10px;
+  margin-top:22px;
+}
+.md-button{
+  position:relative;
+  float:left;
+  margin-left:16px;
+  margin-top:20px;
+  background-color:rgb(68,138,255);
+  color:white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(BootstrapVue)
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 new Vue({
