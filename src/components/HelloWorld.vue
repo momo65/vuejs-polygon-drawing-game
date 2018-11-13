37fe53914b2af0967acdb299cda025d85b35c778
momo65 <mohamed.zerzeri2@gmail.com>
2018-11-13 18:44:50 +0100

    made the application the most stable as possible for now, there are some bugs sometimes, there are too much cases possible couldn't cover them all

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,37 +25,67 @@
 
 <script>
 import {_} from 'vue-underscore';
+const math=require("mathjs")
 
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },methods:{
-    distance:function(pt1,pt2){
-      let a=pt1.x-pt2.x
-      let b=pt1.y-pt2.y
-      return (Math.sqrt(a*a+b*b))
-    },
-    findAngle:function(A,B,C){
-      let AB = Math.sqrt(Math.pow(B.x-A.x,2)+ Math.pow(B.y-A.y,2));
-      let BC = Math.sqrt(Math.pow(B.x-C.x,2)+ Math.pow(B.y-C.y,2));
-      let AC = Math.sqrt(Math.pow(C.x-A.x,2)+ Math.pow(C.y-A.y,2));
-      return Math.acos((BC*BC+AB*AB-AC*AC)/(2*BC*AB));
-    },
-    displayPs:function(){
-      for(let i=0;i<this.$data.points.length;i++){
-        console.log("point "+i+":",this.$data.points[i])
+    calculateCollision:function(p0,p1,p2,p3){
+      let s1_x, s1_y, s2_x, s2_y
+      s1_x = p1.x - p0.x
+      s1_y = p1.y - p0.y
+      s2_x = p3.x - p2.x
+      s2_y = p3.y - p2.y
+      let s, t
+      s = (-s1_y * (p0.x - p2.x) + s1_x * (p0.y - p2.y)) / (-s2_x * s1_y + s1_x * s2_y)
+      t = ( s2_x * (p0.y - p2.y) - s2_y * (p0.x - p2.x)) / (-s2_x * s1_y + s1_x * s2_y)
+      let collisionP={x:null,y:null}
+      if (s >= 0 && s <= 1 && t >= 0 && t <= 1){ // Collision detected
+        collisionP.x = p0.x + (t * s1_x)
+        collisionP.y = p0.y + (t * s1_y)
+      }else{
+        collisionP=null
       }
+      return collisionP
+    },
+    distance:function(pt1,pt2){
+      return math.distance([pt1.x,pt1.y],[pt2.x,pt2.y])
+    },
+    angleIsTPI:function(A,B,C){ //angle==2*PI
+      if(((Math.abs(this.findAngle(A,B,C))>=(47/24)*Math.PI) && (Math.abs(this.findAngle(A,B,C))<=(49/24)*Math.PI))
+        || (this.findAngle(A,B,C)>=((-Math.PI)/24) && (this.findAngle(A,B,C)<=((Math.PI)/24)))){
+          //neglet a value of PI/24 to decrease some precision
+        return true
+      }else{
+        return false
+      }
+    },
+    angleIsPI:function(A,B,C){
+      if((Math.abs(this.findAngle(A,B,C))>=Math.PI*(23/24)) && (Math.abs(this.findAngle(A,B,C))<=Math.PI*(25/24))){
+        //neglet a value of PI/24 to decrease some precision
+        return true
+      }else{
+        return false
+      }
+    },
+    findAngle:function(p0,p1,p2) {//p1 is the center of the angle
+      let a = Math.pow(p1.x-p0.x,2) + Math.pow(p1.y-p0.y,2),
+          b = Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2),
+          c = Math.pow(p2.x-p0.x,2) + Math.pow(p2.y-p0.y,2)
+      return Math.acos( (a+b-c) / Math.sqrt(4*a*b) )
     },
     updatePolygon:function(indexP2,k){
       this.$data.points.splice(k+1,0,this.$data.points[indexP2])
       this.$data.points.splice(indexP2+1,1) //to delete the point that was in indexP2
-      this.drawLine2(k) //each line by itself to check each intersection of these lines with polygon
-      setTimeout(()=>
+      this.drawLine2(k)//draws the line between the k & k+1 points
+      //each line by itself to check each intersection of these lines with polygon
+      setTimeout(()=>{
         this.drawLine2(k+1)
-        },48)
+        },10)
     },
-    checkExtrimities:function(collisionP,pt1,pt2){
+    checkExtremities:function(collisionP,pt1,pt2){
       if(this.isEquivalent(collisionP,pt1) || this.isEquivalent(collisionP,pt2)){
         return true
       }else{
@@ -64,50 +94,87 @@ export default {
     },
     checkIntersection:function(point1,point2,indexP1,indexP2){ //point1 & point2 the 2 extremities of the line
         //to check its intersection with the polygon
-      let collisionX=0,collisionY=0
+      let updateMode=this.$data.updateMode
       if(this.$data.index>=3){
+        let intersections=[],distances=[]
+        let noInters=[] //array for the segments, that doesn't intersect with the new line
         for(let k=0;k<this.$data.points.length-1;k++){
           if(k!=indexP1){ //to avoid to test on the last drawn line
-            let pt1=this.$data.points[k],pt2=this.$data.points[k+1]
-            console.log(point1,point2,pt1,pt2)
-            let x1=point1.x,y1=point1.y,x2=point2.x,y2=point2.y,x3=pt1.x,y3=pt1.y,x4=pt2.x,y4=pt2.y
-            let s1X=x2-x1,s1Y=y2-y1,s2X=x4-x3,s2Y=y4-y3
-            let s=(-s1Y*(x1-x3)+s1X*(y1-y3))/(-s2X*s1Y+s1X*s2Y)
-            let t=(s2X*(y1-y3)-s2Y*(x1-x3))/(-s2X*s1Y+s1X*s2Y)
-            if(s<=1 && s>=0 && t<=1 && t>=0){//collision detected
-              collisionX=x1+(t*s1X)
-              collisionY=y1+(t*s1Y)
-              let collisionP={x:collisionX,y:collisionY}
-              if(!this.checkExtrimities(collisionP,pt1,pt2)){//the intersection happens inside the line not in extremities
-                console.log("k:",k)
-                this.$data.elements[k].remove()
-                setTimeout(()=>{
-                this.updatePolygon(indexP2,k)
-                return true
-                },3000)
+            let pt1=this.$data.points[k],
+            pt2=this.$data.points[k+1]
+            let collisionP=this.calculateCollision(point1,point2,pt1,pt2)
+            if(collisionP!=null){//collision detected
+              if(!this.checkExtremities(collisionP,pt1,pt2)){
+                  //the intersection happens inside the line not in extremities
+                intersections.push(k)
+                distances.push(math.distance([point2.x,point2.y],[collisionP.x,collisionP.y]))
+                //calculate distance between the new point & the intersected line
               }
-            }else{ //the 2 lines are paralell
-              if(this.isEquivalent(point1,pt2) && this.isEquivalent(point2,pt1)){//case where the new line is the inverse of
+            }else{
+              noInters.push(k)
+            }
+          }
+        }
+        if(intersections.length!=0){
+          let z=0
+          let dis=distances[0],disIndex=0
+          while(z<distances.length){
+            z++
+            if(distances[z]<=dis){
+              if(intersections[z]>=this.$data.elements.length){
+                this.intersections.splice(z,1)
+                this.distances.splice(z,1)
+              }else{
+                dis=distances[z]
+                disIndex=z
+              }
+            }
+          }
+          if(!updateMode){
+            this.$data.elements[intersections[disIndex]].remove() //intersections[disIndex] contains the index of the
+              //first point in the nearest line to the new point
+            this.$data.elements.splice(intersections[disIndex],1) //splice because .remove doesn't get rid of the element
+          }
+          this.updatePolygon(indexP2,intersections[disIndex])
+          return true
+        }else{ // there are no intersections with the new line
+          let p1,p2
+          for(let j=0;j<noInters.length;j++){
+            p1=this.$data.points[noInters[j]]
+            p2=this.$data.points[noInters[j]+1]
+            if(this.angleIsTPI(p1,p2,point2) || this.angleIsPI(p1,p2,point2)){ //the 2 lines are paralell
+              if((this.isEquivalent(point1,p2) && this.isEquivalent(point2,p1)) ||
+                (this.isEquivalent(point1,p1) && this.isEquivalent(point2,p2))){//case where the new line is the inverse of
                   //the previous line, it goes upon the previous line & is identical to it, we erase the new point
-                this.$data.elements[k].remove()
-                this.$data.points.splice(k+1,1)
+                if(!updateMode){
+                  this.$data.elements[noInters[j]].remove()
+                  this.$data.elements.splice(noInters[j],1)
+                }
+                this.$data.points.splice(noInters[j]+1,1)
+                this.$data.index-- //because we got rid of one point
                 return false
-              }else if(this.isEquivalent(point1,pt2)){
-                if(this.findAngle(pt2,point2,pt1)==0 || Math.abs(this.findAngle(pt2,point2,pt1))==2*Math.PI){
-                  if(this.distance(pt2,point2)<this.distance(pt2,pt1)){
+              }else if(this.isEquivalent(point1,p2)){
+                if(this.angleIsTPI(p1,p2,point2)){
+                  if(this.distance(p2,point2)<this.distance(p2,p1)){
                     this.$data.points.splice(indexP2,1)
-                    this.$data.elements[indexP2-1].remove()
+                    if(!updateMode){
+                      this.$data.elements[indexP2-1].remove()
+                      this.$data.elements.splice(indexP2-1,1)
+                    }
+                    this.$data.index-- //we got rid of indexP2 point, we decrement index
                     return false
                   }else{
                       //the new extremity is going to be linked with the 2 extremities of the previous line
-                    this.$data.elements[k-2].remove()
-                    this.$data.elements[k].remove()
-                    setTimeout(()=>{
-                    this.updatePolygon(indexP2,k-1) //put the new point after the k-1 point
+                    if(!updateMode){
+                      this.$data.elements[noInters[j]-2].remove()
+                      this.$data.elements.splice(noInters[j]-2,1)
+                      this.$data.elements[noInters[j]].remove()
+                      this.$data.elements.splice(noInters[j],1)
+                    }
+                    this.updatePolygon(indexP2,noInters[j]-1) //put the new point after the noInters[j]-1 point
                     return true
-                    },2600)
                   }
-                }else if(Math.abs(this.findAngle(pt2,point2,pt1))==Math.PI){//the new point's a continuity of the previous
+                }else if(this.angleIsPI(p1,p2,point2)){//the new point's a continuity of the previous
                     //line so we don't need to adjust the polygon
                   return false
                 }
@@ -120,11 +187,12 @@ export default {
     },
     restartGame:function(){//it clears the drawing board & restarts the game
       this.$data.points=[]
-      this.$data.elements=null
+      this.$data.elements=[]
       this.$data.gameOver=false
       this.$data.gameStarted=false
       this.$data.index=0;
       this.$data.strokeW="1"
+      this.$data.updateMode=false
       this.$data.s.clear()
     },
     drawLine:function(event){//it calculates everything to draw the next line & make the necessary verifications
@@ -136,7 +204,7 @@ export default {
         this.$data.points.push(point)
         if(this.$data.index>0){
           let index=this.$data.index
-          console.log("index:",index)
+          this.$data.updateMode=false //(we don't edit polygon)to indicate to checkIntersection() to act differently
           if(!this.checkIntersection(this.$data.points[index-1],point,index-1,index)){
             //if there's no intersection with polygon we draw simple line
             if(this.$data.points[index]!=null || this.$data.points[index]!=undefined
@@ -158,9 +226,10 @@ export default {
       }
     },
     drawLine2:function(index){//it draws an adjustment line & make the necessary verifications for intersections
+      this.$data.updateMode=true //we put it to true so the below function doesn't delete any non yet drawn lines
       if(!this.checkIntersection(this.$data.points[index],this.$data.points[index+1],index,index+1)){
-        if(this.$data.points[index+1]!=null || this.$data.points[index+1]!=undefined
-          || this.$data.points[index+1]!={}){
+        if(this.$data.points[index+1]!=null && this.$data.points[index+1]!=undefined
+          && this.$data.points[index+1]!={}){
             //if there's no intersection with polygon & the last point wasn't removed we draw simple line
           this.$data.elements.splice(index,0,this.$data.s.line(this.$data.points[index].x,this.$data.points[index].y,
             this.$data.points[index+1].x,this.$data.points[index+1].y).attr({stroke:this.$data.stroke,
@@ -169,16 +238,11 @@ export default {
       }
     },
     checkRadius:function(firstP,lastP){
-        //it checks if the last point is near enough to the first point to decide if we close the pentagon or not
+        //it checks if the last point is near enough to the first point to decide if we close the polygon or not
       if(Math.abs(firstP.x-lastP.x)<=9 && Math.abs(firstP.y-lastP.y)<=9){
           //9 is the max radius of the circle that might contain the closing point
           //the possible equations that i thought about that might calculate the fitting strokeWidth.
         this.$data.strokeW=""+(2*(Math.abs(firstP.x-lastP.x)+Math.abs(firstP.y-lastP.y)))
-          /*if(Math.abs(firstP.x-lastP.x)>= Math.abs(firstP.y-lastP.y)){
-            this.$data.strokeW=""+(2*Math.abs(firstP.x-lastP.x))
-          }else{
-            this.$data.strokeW=""+(2*Math.abs(firstP.y-lastP.y))
-          }*/
         this.adjustPolygon()
         return true
       }else{
@@ -210,8 +274,9 @@ export default {
       strokeW:"1",
       stroke:"black",
       gameStarted:false,
+      updateMode:false, //to put on the update polygon's mode or not
       colors:['Black','Green','Red','Blue','Brown','Violet','Orange','Yellow',  //possible colors to draw with
-        'Aqua','Aquamarine','Azure','Crimson','Cyan','DarkBlue','DarkCyan','DarkGoldenRod',
+        'Aqua','Aquamarine','Crimson','Cyan','DarkBlue','DarkCyan','DarkGoldenRod',
         'FireBrick','FloralWhite','ForestGreen','Fuchsia','Gainsboro','Khaki','Lavender','LavenderBlush','LawnGreen','LemonChi',
         'MediumSeaGreen','MediumSlateBlue','MediumSpringGreen','MediumTurquoise','MediumVioletRed','PaleGreen','PaleTurquoise',
         'PaleVioletRed','PapayaWhip','PeachPu','Peru']
